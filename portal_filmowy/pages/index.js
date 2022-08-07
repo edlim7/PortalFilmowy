67805ccb91cabf212142b014ce9f64cb6217c242
Edlim <fureka7@gmail.com>
2022-08-08 00:07:39 +0200

    pog

--- portal_filmowy/pages/index.js
+++ portal_filmowy/pages/index.js
@@ -1,6 +1,14 @@
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
 import Footer from "../components/Footer";
+import https from 'https'
+const agent = new https.Agent({
+  rejectUnauthorized: false
+})
+
+
+
+
 export default function Home() {
 	return (
 		<>
