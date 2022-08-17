61591c259a076ef907a0bc2df649ee3f629f5f25
Edlim <fureka7@gmail.com>
2022-08-17 23:06:37 +0200

    gita

--- portal_filmowy/pages/filmy.js
+++ portal_filmowy/pages/filmy.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
 import Footer from "../components/Footer";
+import SingleContent from "../components/SingleContent/SingleContent";
 export async function getStaticProps() {
 	// Call an external API endpoint to get posts
 	const res = await fetch("http://localhost:5000/api/ProdukcjaKontroler/getAllProdukcja");
@@ -17,16 +18,14 @@ export async function getStaticProps() {
 }
 const Filmy = ({posts}) => {
 const [dataValues, setDataValues] = useState(posts);
-
+console.log(dataValues);
 	return (
 		<>
 			<Navbar></Navbar>
 			<Search></Search>
-		<ul>
-      {dataValues.map((post) => (
-				<li key={post.produkcjaId}>{post.nazwa}</li>
+			{dataValues.map((post) => (
+				<SingleContent key={post.id} nazwa={post.nazwa}  />
       ))}
-    </ul>
 			<Footer></Footer>
 		</>
 	);
